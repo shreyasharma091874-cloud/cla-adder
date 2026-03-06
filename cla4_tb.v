4f77d5e988e381090e90bd1acf63b90c15755fbe
shreyasharma091874-cloud <shreyasharma091874@gmail.com>
2026-03-06 10:50:00 +0530

    Add testbench for 4-bit carry lookahead adder

---
+++ cla4_tb.v
@@ -0,0 +1,27 @@
+module test;
+
+reg [3:0] A;
+reg [3:0] B;
+reg Cin;
+
+wire [3:0] Sum;
+wire Cout;
+
+cla4 uut(A,B,Cin,Sum,Cout);
+
+initial begin
+
+A=4'b0011; B=4'b0101; Cin=0;
+#10
+
+A=4'b1010; B=4'b0101;
+#10
+
+A=4'b1111; B=4'b0001;
+
+#10
+$finish;
+
+end
+
+endmodule
