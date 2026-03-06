c0ab00c3c84e15b09ff7238504be02becb2080ad
shreyasharma091874-cloud <shreyasharma091874@gmail.com>
2026-03-06 12:19:08 +0530

    Update cla4_tb.v

--- cla4_tb.v
+++ cla4_tb.v
@@ -11,6 +11,9 @@ cla4 uut(A,B,Cin,Sum,Cout);
 
 initial begin
 
+$dumpfile("wave.vcd");
+$dumpvars(0,test);
+
 A=4'b0011; B=4'b0101; Cin=0;
 #10
 
@@ -18,8 +21,8 @@ A=4'b1010; B=4'b0101;
 #10
 
 A=4'b1111; B=4'b0001;
-
 #10
+
 $finish;
 
 end
