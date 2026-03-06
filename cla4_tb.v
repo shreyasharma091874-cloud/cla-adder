d654fb3572c4462a2e919d197b426e1baa77402b
shreyasharma091874-cloud <shreyasharma091874@gmail.com>
2026-03-06 12:26:51 +0530

    Refactor testbench assignments for clarity

--- cla4_tb.v
+++ cla4_tb.v
@@ -14,14 +14,14 @@ initial begin
 $dumpfile("wave.vcd");
 $dumpvars(0,test);
 
-A=4'b0011; B=4'b0101; Cin=0;
-#10
+A = 4'b0011; B = 4'b0101; Cin = 0;
+#10;
 
-A=4'b1010; B=4'b0101;
-#10
+A = 4'b1010; B = 4'b0101;
+#10;
 
-A=4'b1111; B=4'b0001;
-#10
+A = 4'b1111; B = 4'b0001;
+#10;
 
 $finish;
 
